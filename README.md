c3981218ee0a06751564e94e4a9a0f2e4b3a13eb
MidrarAdham <midrar@pdx.edu>
2025-07-19 11:08:34 -0700

    a working example of a 4-node feeder. Check the readme for progress update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Quick Progress Overview
 So far, I resolved some HELICS issues. Here are the important ones:
 
 - [error] broker responded with error: duplicate broker name detected
@@ -7,3 +8,34 @@ This error was caused by an existing operation of HELICS that was not terminated
     - ps aux | grep "helic"
     - Then kill that process:
         - ps kill [PID]
+
+# How to run the GLD-Python example
+Currently, there is one example of HELICS - GridLAB-D integration. Here is how to run it:
+
+- Open three terminal windows.
+- In the first one, run the broker:
+```
+helics_broker -f 2 --loglevel=warning
+```
+- In the second terminal window, run the GridLAB-D federate:
+```
+gridlabd -D HELICS_CONFIG_FILE=powerflow_4node_gld_config.json powerflow_4node.glm
+```
+- In the third terminal window, run the python federate:
+```
+python3 powerflow_test.py
+```
+
+## Progress
+
+- The glm file is a four node feeder. The feeder incorporates inverter-battery object.
+
+- The inverter-battery object is controlled by setting its ```P_Out``` and ```Q_Out``` properties.
+
+- These values are published correctly by the python federate.
+
+- However, the recorder object (attached to the inverter-battery object) is not recording these values.
+
+- I tried adding a ```triplex_load``` object instead, which worked as expected.
+
+- I posted a question in the GridLAB-D forum, see [here](https://sourceforge.net/p/gridlab-d/discussion/842562/thread/e5a93df6c6/).
--- gridlabd_helics_example/python_files/powerflow_test.py
+++ gridlabd_helics_example/python_files/powerflow_test.py
@@ -5,16 +5,17 @@ Created on Thu June 26 09:28 a.m.
 References: 1abc_Transmission_simulator.py by Monish.Mukherjee
 """
 
-import scipy.io as spio
-from pypower.api import case118, ppoption, runpf, runopf
 import math
 import numpy
-import matplotlib.pyplot as plt
 import time
-import helics as h
 import random
 import logging
 import argparse
+import helics as h
+import pandas as pd
+import scipy.io as spio
+import matplotlib.pyplot as plt
+from pypower.api import case118, ppoption, runpf, runopf
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -35,19 +36,19 @@ def initialize_execute_federate (fed):
     """
     h.helicsFederateEnterInitializingMode(fed) # Initialization
     status = h.helicsFederateEnterExecutingMode(fed)
-    pass
+    return status
 
 def publications_handling (fed):
     pubkeys_count = h.helicsFederateGetPublicationCount(fed)    #Get the number of publication in each federate
     subkeys_count = h.helicsFederateGetInputCount(fed)  # Get the number of subscriptions in a federate.
     return pubkeys_count, subkeys_count
 
-def time_synchronization_federate ():
+def get_power_info (filename):
     """
-    needed to sync with GLD every time step.
+    Reads a csv file and returns a list of the needed values
     """
-    grantedtime = h.helicsFederateRequestTime(fed, h.HELICS_TIME_MAXTIME)
-    pass
+    df = pd.read_csv(filename)
+    return df['time'].values ,df['watts'].values
 
 def destroy_federate(fed):
     status = h.helicsFederateDisconnect(fed)
@@ -66,13 +67,74 @@ if __name__ == "__main__":
     default_path = "/home/deras/gld-opedss-ochre-helics/gridlabd_helics_example"
     py_federate_config_file = f"{default_path}/python_files/powerflow_4node_config_py.json"
     gld_federate_config_file = f"{default_path}/gld_files/powerflow_4node_config_gld.json"
+    filename = "../csv_profiles/der_profile_updated.csv"
     
-    print("I am here 0")
+    print('going into create federate function')
     fed = create_federate (py_federate_config_file)
-    print(fed)
-    initialize_execute_federate (fed)
-    print("I am here 2")
+
+    print('going into initialize_execute_federate')
+    status = initialize_execute_federate (fed)
+    print(f"Initialization status: {status}")
+    
+    print('going into publications_handling')
     pubkeys_count, subkey_count = publications_handling (fed)
-    print("I am here 4")
-    print(f"fed {fed}\nfederate name: none for now\npublications in federate: {pubkeys_count}\nsubscriptions in federate: {subkey_count}")
-    print("I am here 5")
+    print(f"Publications: {pubkeys_count}, Subscriptions: {subkey_count}")
+    
+    print("going into helicsFederateGetPublicationByIndex")
+    pubid = h.helicsFederateGetPublicationByIndex(fed, 0)
+    
+    # Get publication info for debugging
+    pub_key = h.helicsPublicationGetKey(pubid)
+    pub_type = h.helicsPublicationGetType(pubid)
+    print(f"Publication key: {pub_key}")
+    print(f"Publication type: {pub_type}")
+    
+    print("getting the power info from the csv file")
+    sim_time, watts = get_power_info(filename)
+
+    grantedtime = 0
+    interval = 60
+    total_steps = len(watts)
+
+    start = time.time()    
+    print("going into the loop")
+    for t in range(total_steps):
+        power_val = watts[t]
+        time_val = sim_time[t]
+        
+        print(f"Step {t}: time={time_val}, power={power_val}")
+        
+        print(f"Publishing power value: {power_val}")
+        try:
+            h.helicsPublicationPublishDouble(pubid, float(power_val))
+            print("Publication successful")
+        except Exception as e:
+            print(f"Publication failed: {e}")
+        
+        # Verify the publication was successful
+        if h.helicsPublicationIsValid(pubid):
+            print("Publication is valid")
+        else:
+            print("WARNING: Publication is not valid!")
+
+        # Sync time
+        requested_time = (t+1)*interval
+        print(f"Requesting time: {requested_time}")
+        grantedtime = h.helicsFederateRequestTime(fed, requested_time)
+        print(f"Granted time: {grantedtime}")
+
+        # Check if we're synchronized properly
+        if grantedtime < requested_time:
+            print(f"WARNING: Granted time {grantedtime} is less than requested time {requested_time}")
+
+        end = time.time()
+        elapsed = end - start
+        print(f'Elapsed time: {elapsed:.2f} seconds')
+        
+        if elapsed >= 300:
+            print("Stopping the simulation after 5 minutes")
+            break
+
+    print("destroying the federates")
+    h.helicsFederateDisconnect(fed)
+    destroy_federate(fed)
