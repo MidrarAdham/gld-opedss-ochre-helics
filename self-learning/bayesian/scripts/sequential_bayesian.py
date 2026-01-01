1f833ff08d132597884bd33573a31ac116af7349
MidrarAdham <midrar@pdx.edu>
2025-12-31 19:53:08 -0800

    Tried different hyperparameters. Now I want to learn seq. bayesian

---
+++ self-learning/bayesian/scripts/sequential_bayesian.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+from scipy.special import comb
+from scipy.special import beta, binom
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
